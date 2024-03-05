2fa8e76888ee0483854de8e9b2dff2216dbec3e4
Thoe3840 <theofmichell@gmail.com>
2024-03-05 00:40:45 +0000

    Implement basic board class

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import pygame
 from pygame import *
 
+from board import Board
+
 DISPLAY_WIDTH = 700
 DISPLAY_HEIGHT = 600
+WIDTH = 7
+HEIGHT = 6
 
 pygame.init()
 
 DISP = display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 display.set_caption("Connect 4 Recursive")
+
+# testing board class
+board = Board(WIDTH, HEIGHT, 2)
+print(board)
+print(board.chips)
+print(board.sub_boards)
 
 running = True
 while running:

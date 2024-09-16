d51d970468c221e765ab94c8b1465ab84b1c7023
Thoe3840 <theofmichell@gmail.com>
2024-09-16 05:36:02 +0100

    Add highlight for selected column
    When the user mouses over a column, it should indicate where the
    next chip would be placed by changing the colour of the
    corresponding empty space. This should stop happening when the board is
    completed.

--- main.py
+++ main.py
@@ -13,18 +13,27 @@ board = Board(WIDTH, HEIGHT, 1, 4)
 view = View(WIDTH, HEIGHT)
 view.render_board(board)
 
+column = -1
 player = True  # player 1: T, player 2: F
 running = True
 while running:
+  previous_column = column
+  column = view.which_column_selected(mouse.get_pos()[0])
+
   for event in pygame.event.get():
     if event.type == QUIT:
       running = False
       break
 
-    elif event.type == MOUSEBUTTONDOWN and event.button == 1:  # left click
-      column = view.which_column_clicked(mouse.get_pos()[0])
-      if column != -1 and board.can_play_here(column):
-        row = board.place_chip(column, player)
-        view.update_chip(column, row, player)
-        player = not player
-        status = board.is_completed(column, row)
+    # left click on column with a space
+    elif event.type == MOUSEBUTTONDOWN and event.button == 1 and column != -1 and board.can_play_here(column):
+      row = board.find_lowest_empty_row(column)
+      board.set_chip(column, row, player)
+      view.update_chip(column, row, player)
+      player = not player
+      board.update_status(column, row)
+
+  # update selected column highlights
+  if column != previous_column and board.complete == Board.NOT_COMPLETE:
+    if column != -1: view.highlight_selected_columnn(column, board.find_lowest_empty_row(column), True)
+    if previous_column != -1: view.highlight_selected_columnn(previous_column, board.find_lowest_empty_row(previous_column), False)
